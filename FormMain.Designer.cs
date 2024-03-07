07b8f3b027aff872a4a9c2e5df8755ac60f7c336
Don <496156+nikolat@users.noreply.github.com>
2024-03-07 20:32:30 +0900

    changed target framework to .NET 8.0

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -29,85 +29,81 @@ namespace DirectSSTPTester
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBoxMessageToSend = new System.Windows.Forms.TextBox();
-            this.comboBoxTargetName = new System.Windows.Forms.ComboBox();
-            this.buttonRefreshFMO = new System.Windows.Forms.Button();
-            this.textBoxMessageReceived = new System.Windows.Forms.TextBox();
-            this.buttonSend = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            textBoxMessageToSend = new System.Windows.Forms.TextBox();
+            comboBoxTargetName = new System.Windows.Forms.ComboBox();
+            buttonRefreshFMO = new System.Windows.Forms.Button();
+            textBoxMessageReceived = new System.Windows.Forms.TextBox();
+            buttonSend = new System.Windows.Forms.Button();
+            SuspendLayout();
             // 
             // textBoxMessageToSend
             // 
-            this.textBoxMessageToSend.AllowDrop = true;
-            this.textBoxMessageToSend.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxMessageToSend.Location = new System.Drawing.Point(12, 12);
-            this.textBoxMessageToSend.Multiline = true;
-            this.textBoxMessageToSend.Name = "textBoxMessageToSend";
-            this.textBoxMessageToSend.Size = new System.Drawing.Size(948, 346);
-            this.textBoxMessageToSend.TabIndex = 0;
-            this.textBoxMessageToSend.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextBoxMessageToSend_DragDrop);
-            this.textBoxMessageToSend.DragEnter += new System.Windows.Forms.DragEventHandler(this.TextBoxMessageToSend_DragEnter);
+            textBoxMessageToSend.AllowDrop = true;
+            textBoxMessageToSend.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            textBoxMessageToSend.Location = new System.Drawing.Point(12, 12);
+            textBoxMessageToSend.Multiline = true;
+            textBoxMessageToSend.Name = "textBoxMessageToSend";
+            textBoxMessageToSend.Size = new System.Drawing.Size(949, 346);
+            textBoxMessageToSend.TabIndex = 0;
+            textBoxMessageToSend.DragDrop += TextBoxMessageToSend_DragDrop;
+            textBoxMessageToSend.DragEnter += TextBoxMessageToSend_DragEnter;
             // 
             // comboBoxTargetName
             // 
-            this.comboBoxTargetName.FormattingEnabled = true;
-            this.comboBoxTargetName.Location = new System.Drawing.Point(12, 371);
-            this.comboBoxTargetName.Name = "comboBoxTargetName";
-            this.comboBoxTargetName.Size = new System.Drawing.Size(272, 45);
-            this.comboBoxTargetName.TabIndex = 1;
+            comboBoxTargetName.FormattingEnabled = true;
+            comboBoxTargetName.Location = new System.Drawing.Point(12, 371);
+            comboBoxTargetName.Name = "comboBoxTargetName";
+            comboBoxTargetName.Size = new System.Drawing.Size(272, 45);
+            comboBoxTargetName.TabIndex = 1;
             // 
             // buttonRefreshFMO
             // 
-            this.buttonRefreshFMO.Location = new System.Drawing.Point(290, 364);
-            this.buttonRefreshFMO.Name = "buttonRefreshFMO";
-            this.buttonRefreshFMO.Size = new System.Drawing.Size(169, 52);
-            this.buttonRefreshFMO.TabIndex = 2;
-            this.buttonRefreshFMO.Text = "更新";
-            this.buttonRefreshFMO.UseVisualStyleBackColor = true;
-            this.buttonRefreshFMO.Click += new System.EventHandler(this.ButtonRefreshFMO_Click);
+            buttonRefreshFMO.Location = new System.Drawing.Point(290, 364);
+            buttonRefreshFMO.Name = "buttonRefreshFMO";
+            buttonRefreshFMO.Size = new System.Drawing.Size(169, 52);
+            buttonRefreshFMO.TabIndex = 2;
+            buttonRefreshFMO.Text = "更新";
+            buttonRefreshFMO.UseVisualStyleBackColor = true;
+            buttonRefreshFMO.Click += ButtonRefreshFMO_Click;
             // 
             // textBoxMessageReceived
             // 
-            this.textBoxMessageReceived.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxMessageReceived.Location = new System.Drawing.Point(12, 422);
-            this.textBoxMessageReceived.Multiline = true;
-            this.textBoxMessageReceived.Name = "textBoxMessageReceived";
-            this.textBoxMessageReceived.ReadOnly = true;
-            this.textBoxMessageReceived.Size = new System.Drawing.Size(948, 287);
-            this.textBoxMessageReceived.TabIndex = 4;
+            textBoxMessageReceived.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            textBoxMessageReceived.Location = new System.Drawing.Point(12, 422);
+            textBoxMessageReceived.Multiline = true;
+            textBoxMessageReceived.Name = "textBoxMessageReceived";
+            textBoxMessageReceived.ReadOnly = true;
+            textBoxMessageReceived.Size = new System.Drawing.Size(949, 287);
+            textBoxMessageReceived.TabIndex = 4;
             // 
             // buttonSend
             // 
-            this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonSend.Location = new System.Drawing.Point(791, 366);
-            this.buttonSend.Name = "buttonSend";
-            this.buttonSend.Size = new System.Drawing.Size(169, 52);
-            this.buttonSend.TabIndex = 3;
-            this.buttonSend.Text = "送信";
-            this.buttonSend.UseVisualStyleBackColor = true;
-            this.buttonSend.Click += new System.EventHandler(this.ButtonSend_Click);
+            buttonSend.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            buttonSend.Location = new System.Drawing.Point(792, 366);
+            buttonSend.Name = "buttonSend";
+            buttonSend.Size = new System.Drawing.Size(169, 52);
+            buttonSend.TabIndex = 3;
+            buttonSend.Text = "送信";
+            buttonSend.UseVisualStyleBackColor = true;
+            buttonSend.Click += ButtonSend_Click;
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(972, 721);
-            this.Controls.Add(this.buttonSend);
-            this.Controls.Add(this.textBoxMessageReceived);
-            this.Controls.Add(this.buttonRefreshFMO);
-            this.Controls.Add(this.comboBoxTargetName);
-            this.Controls.Add(this.textBoxMessageToSend);
-            this.MinimumSize = new System.Drawing.Size(800, 600);
-            this.Name = "FormMain";
-            this.Text = "DirectSSTPTester";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMain_FormClosed);
-            this.Load += new System.EventHandler(this.FormMain_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(972, 721);
+            Controls.Add(buttonSend);
+            Controls.Add(textBoxMessageReceived);
+            Controls.Add(buttonRefreshFMO);
+            Controls.Add(comboBoxTargetName);
+            Controls.Add(textBoxMessageToSend);
+            MinimumSize = new System.Drawing.Size(800, 600);
+            Name = "FormMain";
+            Text = "DirectSSTPTester";
+            FormClosed += FormMain_FormClosed;
+            Load += FormMain_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 ========================================================================
 【ソフト名称】DirectSSTPTester
 【 制 作 者 】Don
-【 動作環境 】Windows11 22H2 + .NET 7.0
+【 動作環境 】Windows11 23H2 + .NET 8.0
 【 タ イ プ 】DirectSSTP送信テストツール
 【 取扱種別 】フリーウェア
 【 配布月日 】2021/06/08
@@ -23,18 +23,11 @@
 
 ■ライセンス
 
-  ukiya氏のSSTPLibを一部改変して利用しております。
-  ありがとうございます。
-
-    SSTPLib
-      http://ukiya.sakura.ne.jp/index.php?%E4%BC%BA%E3%81%8B%E9%96%A2%E9%80%A3%E3%83%84%E3%83%BC%E3%83%AB%2FSSTPLib
-
-  上記以外のコードのライセンスは「CC0 1.0 Universal」です。
-
-    Creative Commons - CC0 1.0 Universal
-      http://creativecommons.org/publicdomain/zero/1.0/
+  Creative Commons - CC0 1.0 Universal
+    http://creativecommons.org/publicdomain/zero/1.0/
 
 ■更新履歴
+2024/03/07  Ver1.5.0  ・ターゲットフレームワークを.NET 8.0に変更
 2023/02/02  Ver1.4.0  ・ターゲットフレームワークを.NET 7.0に変更
 2022/03/08  Ver1.3.0  ・ゴースト名取得時にSakuraUnicode FMOを使用
 2022/03/02  Ver1.2.0  ・SSTPプロトコル違反の修正、Shift_JIS以外の文字コード対応
